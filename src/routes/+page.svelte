c3956e6ce78659b32c41678d3918bf7993f182b5
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2026-04-03 21:34:26 +0300

    feat: add Hero component with lucide icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-	import type { PageProps } from './$types';
-	let { data }: PageProps = $props();
+	import Hero from '$lib/components/Hero.svelte';
 </script>
 
 <div class="mx-auto max-w-6xl py-4">
-	{#each data.projects as project (project.id)}
-		<div>
-			<span>{project.title}</span>
-			<span>{project.status}</span>
-		</div>
-	{/each}
+	<Hero />
 </div>
